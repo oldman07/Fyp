b876c396996dfe71e0d58d87f172b155fb376431
oldman07 <backagain010@gmail.com>
2023-05-30 20:46:58 +0500

    Adding prduct controller,model etc

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
+
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ShippingController;
+use App\Http\Controllers\ImageSearchController;
+use App\Http\Controllers\FlaskDataController;
+use App\Http\Controllers\VoiceSearchController;
+use App\Http\Controllers\VideoController;
+use App\Http\Controllers\blendermodelController;
+use App\Http\Controllers\VisualController;
+use App\Http\Controllers\BotManChatController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,22 +20,75 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//home page
+Route::get('/', [ProductController::class,'home_view'])->name('home');
+
+
+//shop page
+Route::get('/shop',[ProductController::class,'view']);
+
+
+Route::get('/show-model/', [ProductController::class, 'showModel'])->name('show_model');
+
+
+Route::get('/speed_form', function () {
+    return view('speed_form');
 });
 
-Route::get('/about-us', function () {
-    return view('frontend/about-us');
+
+Route::get('/image-search',[ImageSearchController::class,'index']);
+
+
+
+
+Route::get('/individual/{id}', [ProductController::class,'show']);
+// Route::get('/cart/{id}', [ProductController::class,'show_cart']);
+
+// Route::get('/cart', function () {
+//     return view('shop/cartpage');
+// });
+
+Route::get('/shipping', function () {
+    return view('shop/shipping');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/product', function () {
+    return view('backend/product_add');
+})->middleware(['auth', 'verified'])->name('product_add');
+
+Route::get('/edit_video', function () {
+    return view('backend/edit_video');
+})->middleware(['auth', 'verified'])->name('edit_video');
+
+Route::get('/edit_3dmodel', function () {
+    return view('backend/edit_3dmodel');
+})->middleware(['auth', 'verified'])->name('edit_3dmodel');
+
+
+
+//adding the product to database
+Route::post('/product', [ProductController::class,'store']);
+
+
+
+Route::get('/shipping-confirm', function () {
+    echo  'thanks for shopping';
+});
+
+Route::post('/shipping-confirm', [ShippingController::class,'store']);
+
+Route::get('/register', function () {
+    return view('auth/register');
+})->middleware(['auth', 'verified'])->name('register');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -32,4 +96,64 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::post('/visual-search', [VisualController::class,'search']);
+
+
+
+Route::post('/process', [blendermodelController::class,'process'] )->name('process');
+
+Route::post('/process-video', [VideoController::class, 'processVideo'])->name('process-video');
+
+Route::get('/process-video', function () {
+    return view('process_video');
+});
+
+
+
+
+Route::get('/render-blender-video', [blendermodelController::class, 'renderBlenderVideo']);
+
+Route::get('/create-blender-video', [blendermodelController::class, 'createBlenderVideo']);
+
+//3D model upload
+Route::post('/upload-gltf', [blendermodelController::class, 'uploadGltf']);
+
+
+Route::get('/run-python', [VideoController::class, 'runPythonScript']);
+
+Route::get('/voice-search',[VoiceSearchController::class,'index']);
+Route::get('/shop1', [VoiceSearchController::class, 'showTranscription']);
+
+Route::post('/transcription', [VoiceSearchController::class, 'transcribeAudio']);
+
+Route::get('/test',function () {
+    return view('test');
+});
+
+Route::get('/view3D/{id}', [blendermodelController::class, 'show']);
+
+
+Route::get('/run-blender-script' ,  [FlaskDataController::class, 'runBlenderScript']);
+
+
 require __DIR__.'/auth.php';
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/about-us', function () {
+//     return view('frontend/about-us');
+// });
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// require __DIR__.'/auth.php';

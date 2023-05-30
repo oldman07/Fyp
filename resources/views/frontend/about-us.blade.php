e1f26354ac1d59a450960879177a7991fac219d5
oldman07 <backagain010@gmail.com>
2023-05-30 20:22:20 +0500

    bootstrap added + breeze

---
+++ resources/views/frontend/about-us.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <title>About Us Page</title>
+</head>
+<body>
+   <div class="container">
+    <h1 class="text-center mt-5">About Us</h1>
+    <div class="row">
+        <div class="col-md-6">
+            <img src="your-company-logo.png" alt="Company Logo" class="img-fluid rounded-circle">
+        </div>
+        <div class="col-md-6">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras porttitor metus in metus ullamcorper, et bibendum nisl tempus. Donec bibendum lectus ut lectus ultrices, et cursus erat justo. Sed pellentesque velit ut odio placerat tincidunt. Aenean vel dolor vel erat scelerisque, eu facilisis libero. Integer vel nisl vitae tellus elementum, et bibendum auctor elit. Donec consequat, metus at volutpat gravida, metus laoreet cursus eleifend, vitae ultricies odio a mi.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <h2 class="text-center">Our Vision</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras porttitor metus in metus ullamcorper, et bibendum nisl tempus. Donec bibendum lectus ut lectus ultrices, et cursus erat justo. Sed pellentesque velit ut odio placerat tincidunt. Aenean vel dolor vel erat scelerisque, eu facilisis libero. Integer vel nisl vitae tellus elementum, et bibendum auctor elit. Donec consequat, metus at volutpat gravida, metus laoreet cursus eleifend, vitae ultricies odio a mi.</p>
+        </div>
+        <div class="col-md-6">
+            <h2 class="text-center">Our Mission</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras porttitor metus in metus ullamcorper, et bibendum nisl tempus. Donec bibendum lectus ut lectus ultrices, et cursus erat justo. Sed pellentesque velit ut odio placerat tincidunt. Aenean vel dolor vel erat scelerisque, eu facilisis libero. Integer vel nisl vitae tellus elementum, et bibendum auctor elit. Donec consequat, metus at volutpat gravida, metus laoreet cursus eleifend, vitae ultricies odio a mi.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <h2 class="text-center">Our Team</h2>
+            <!-- Add team members and their profiles here -->
+        </div>
+        <div class="col-md-6">
+            <h2 class="text-center">Contact Information</h2>
+            <!-- Add your company's contact information here -->
+        </div>
+    </div>
+</div>
+
+</body>
+</html>

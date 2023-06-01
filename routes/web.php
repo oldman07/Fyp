a5aec6bc78bc72715c556195b983b91e56003f84
oldman07 <backagain010@gmail.com>
2023-06-01 12:40:41 +0500

    new branch test

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Models\Product;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routesas
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
